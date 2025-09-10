1c874082b2e08e2063aff09358ae86cae738700c
twg041110 <150268649+twg041110@users.noreply.github.com>
2025-09-11 00:25:26 +0900

    Update 두 수의 곱 구하기.c

--- 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.c
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.c
@@ -4,7 +4,7 @@
 
 int solution(int num1, int num2) {
     int answer = 0;
-    if(0<=num1,num2<=100);
+    if(0<=num1 && num1<=100, 0<=num2 && num2<=100);
     {
         answer = num1 * num2;
     }
